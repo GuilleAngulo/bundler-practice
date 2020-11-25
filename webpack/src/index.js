5deef86622dcfb6d1e267d8601233575e7d11d76
Guillermo Angulo <anyermo@gmail.com>
2020-11-25 18:33:38 -0300

    Add production and development plugins support for webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom'
 
 /** SIMPLE */
 
-//ReactDOM.render('Hello World', document.getElementById('app'))
+//ReactDOM.render('Hello', document.getElementById('app'))
 
 /** WITH HTML */
 
 // ReactDOM.render(
-//     React.createElement('h1', null, 'Hola Mundo'),
+//     React.createElement('h1', null, 'Hello World'),
 //     document.getElementById('app')
 // )
 
 /** WITH JSX THROUGH BABEL */
 
 ReactDOM.render(
-    <h1>Hola Mundo 😃</h1>,
+    <h1>Hello World 😃</h1>,
     document.getElementById('app')
 )
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,3 +1,6 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+
 const JSRules = {
     test: /\.js$/,
     exclude: /node_modules/,
@@ -9,8 +12,33 @@ const JSRules = {
     }
 }
 
-module.exports = {
+const developmentPlugins = []
+
+const productionPlugins = [
+    //Gzip
+    new CompressionPlugin(),
+    //Brotli
+    new CompressionPlugin({
+        filename: "[path][base].br",
+        algorithm: "brotliCompress",
+    }),
+]
+
+module.exports = (env, { mode }) => ({
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: 'output.[contenthash].js'
+    },
     module: {
         rules: [JSRules]
-    }
-}
+    },
+    plugins: [
+        ...(mode === 'production' ? productionPlugins : developmentPlugins),
+        new HTMLWebpackPlugin({
+            title: "Practicing Webpack",
+            template: 'src/index.html'
+        })
+    ].filter(Boolean)
+})
