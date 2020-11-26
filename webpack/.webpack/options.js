b1d275a821648984371a85bcb1b014d631f5c03e
Guillermo Angulo <anyermo@gmail.com>
2020-11-26 15:38:05 -0300

    Add caching vendors bundle optimization

--- webpack/.webpack/options.js
+++ webpack/.webpack/options.js
@@ -1,16 +1,41 @@
 const path = require( 'path');
 
+/** Track down source errors | DEVELOPMENT **/
+const devtool = 'inline-source-map'
+
+/** Webpack Dev Server | DEVELOPMENT */
+const devServer = {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    open: true,
+    port: 9000
+}
+
+/** Caching vendors (React) boilerplate bundle */
+const optimization = {
+    runtimeChunk: 'single',
+    splitChunks: {
+        cacheGroups: {
+            vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name: 'vendors',
+                chunks: 'all',
+            },
+        }
+    }
+}
+
 const options = (mode) => {
     console.log(mode)
     if(mode === 'development') {
         return {
-            devtool: 'inline-source-map',
-            devServer: {
-                contentBase: path.join(__dirname, 'dist'),
-                compress: true,
-                open: true,
-                port: 9000
-            },
+            devtool,
+            devServer,
+        }
+    }
+    if(mode === 'production') {
+        return {
+            optimization,
         }
     }
   };
