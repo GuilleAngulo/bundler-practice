4703464a2c07b6e970b691155b96fe80efa10493
Guillermo Angulo <anyermo@gmail.com>
2020-11-26 20:41:02 -0300

    Create esbuild app

---
+++ esbuild/src/app.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Component from './components/Component'
+
+ReactDOM.render(
+    <Component />, 
+    document.getElementById('app')
+)
---
+++ esbuild/src/components/Component.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export default function Component({ message = 'Hello World'}) {
+    return <h1>{message}</h1>
+}
